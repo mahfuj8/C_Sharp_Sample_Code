0cc1834c52e6226e397ed64511e0ec5f5e6851e8
mahfujul8 <mahfujul8@gmail.com>
2017-09-23 20:44:26 +0600

    Add master file

---
+++ C_SharpExample/C_SharpExample/XMLFileWrite.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace C_SharpExample
+{
+    public class XMLFileWrite
+    {
+
+        /* 
+     -------------Method Output output
+       <users>
+       <user age = "42" > John Doe</user>
+       <user age = "39" > Jane Doe</user>
+       </users>
+
+       */
+
+        public void DataToXmlFileWrite()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlNode rootNode = xmlDoc.CreateElement("users");
+            xmlDoc.AppendChild(rootNode);
+
+            XmlNode userNode = xmlDoc.CreateElement("user");
+            XmlAttribute attribute = xmlDoc.CreateAttribute("age");
+            attribute.Value = "42";
+            userNode.Attributes.Append(attribute);
+            userNode.InnerText = "John Doe";
+
+            //add root node
+            rootNode.AppendChild(userNode);
+
+            userNode = xmlDoc.CreateElement("user");
+            attribute = xmlDoc.CreateAttribute("age");
+            attribute.Value = "39";
+            userNode.Attributes.Append(attribute);
+            userNode.InnerText = "Jane Doe";
+
+            //add root node
+            rootNode.AppendChild(userNode);
+
+            xmlDoc.Save(@"G:\xamarin code\test-doc.xml");
+
+        }
+    }
+}
