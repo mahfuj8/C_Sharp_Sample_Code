01fe9e316d1ffd9ee73ca2b040d16166b08084cd
Md.Mahfujul Haque <mahfujul8@gmail.com>
2017-09-25 13:40:11 +0600

    add Custom class create

---
+++ C_SharpExample/C_SharpExample/CustomCollectionCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    public class ModifiedCustomersQueryBO : System.Collections.CollectionBase
+    {
+
+        public ModifiedCustomersQueryBO()
+        {
+            base.InnerList.Clear();
+        }
+
+        public virtual void Add(string item)
+        {
+            base.InnerList.Add(item);
+        }
+
+        public virtual string this[int index]
+        {
+            get
+            {
+                return (string)(base.InnerList[index]);
+            }
+            set
+            {
+
+                base.InnerList[index] = value;
+            }
+        }
+
+    }
+}
